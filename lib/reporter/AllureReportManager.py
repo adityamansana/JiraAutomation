ca9f7717d84b313388cac78bd1e36bfb8b03cc4d
Aditya Mansana <33343968+adityamansana@users.noreply.github.com>
2026-02-07 12:55:47 +0530

    Core Repository Structure

---
+++ lib/reporter/AllureReportManager.py
@@ -0,0 +1,4 @@
+"""
+AllureReportManager is responsible for managing the Allure report generation and configuration.
+It provides methods to set up the Allure environment, generate reports, and handle report-related tasks.
+"""
